a71cabd1a19b2beaf8fdfa50ab00b5419fef9d10
raziyeh bazargan <r.bazargan@gmail.com>
2016-02-20 22:05:25 -0800

    add smothy scroll

--- scripts/loadpage.js
+++ scripts/loadpage.js
@@ -3,3 +3,17 @@ $(window).on('load',function(){
   $('.title').typeIt();
   $('#textPageLaod').slideDown(2000);
 });
+
+// smoth scroll
+$('.scroll').click(function() {
+ if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+   var target = $(this.hash);
+   target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+   if (target.length) {
+     $('html,body').animate({
+       scrollTop: target.offset().top
+     }, 1100); // The number here represents the speed of the scroll in milliseconds
+     return false;
+   }
+ }
+});
