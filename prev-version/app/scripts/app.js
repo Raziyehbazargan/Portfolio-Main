1ef0a5110294bdebe83c4ec4ac30f9cd23bd8ff7
raziyeh bazargan <r.bazargan@gmail.com>
2017-01-21 23:52:54 -0800

    fixed spaces

--- prev-version/app/scripts/app.js
+++ prev-version/app/scripts/app.js
@@ -3,29 +3,28 @@
 angular.module('portfolioApp',['ngRoute'])
 
 	.config(function($routeProvider) {
-        $routeProvider
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'views/about.html',
-                controller  : 'AboutCtrl'
-            })
-		  // route for the home page
-            .when('/home', {
-                templateUrl : 'views/home.html',
-                controller  : 'HomeCtrl'
-            })
-		 // route for the contactus page
-            .when('/contact', {
-                templateUrl : 'views/contact.html',
-                controller  : 'ContactCtrl'
-            })
-		 // route for the projects page
-            .when('/projects', {
-                templateUrl : 'views/projects.html',
-                controller  : 'ProjectsCtrl'
-            })
-		 //default page
-            .otherwise('/home');
-    })
+  $routeProvider
+      // route for the about page
+      .when('/about', {
+        templateUrl : 'views/about.html',
+        controller  : 'AboutCtrl'
+      })
+// route for the home page
+      .when('/home', {
+        templateUrl : 'views/home.html',
+        controller  : 'HomeCtrl'
+      })
+// route for the contactus page
+      .when('/contact', {
+        templateUrl : 'views/contact.html',
+        controller  : 'ContactCtrl'
+      })
+// route for the projects page
+      .when('/projects', {
+        templateUrl : 'views/projects.html',
+        controller  : 'ProjectsCtrl'
+      })
+//default page
+      .otherwise('/home');
+})
 ;
-
